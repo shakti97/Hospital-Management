1eb643fa12f3ded99affbc2c070625d7e9bcdd2b
shakti97 <shaktisingh230797@gmail.com>
2018-10-05 00:54:11 +0530

    Refactoring DashBoard

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,13 @@
 import React,{Component} from 'react';
+import '../CSS/Home.css'
 
 //This is Home Component DashBoard of the Application
 export class HomePage extends Component{
     render(){
         return (
             <div >
-              <img src="//gccontent.blob.core.windows.net/gccontent/blogs/legacy/wijmo/2016/11/01_DynamicDashboard.png" alt="DashBoard"/>
+              <img 
+              className='dashBoard' src="//gccontent.blob.core.windows.net/gccontent/blogs/legacy/wijmo/2016/11/01_DynamicDashboard.png" alt="DashBoard"/>
             </div>
         )
     }
